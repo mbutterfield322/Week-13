713408783bcc8198f21d2c355ade6ae66bcce8fa
Matthew Butterfield <mbutterfield322@gmail.com>
2023-01-17 13:51:50 -0800

    Week 13

---
+++ src/components/navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class Navbar extends React.Component {
+    render () {
+        return (
+<div class='Navbar'>
+<ul>
+<li><a href='#'>Home</a></li>
+  <li><a href='#'>Restaurant</a></li>
+  <li><a href='#'>Wine</a></li>
+  <li><a href='#'>Steaks</a></li>
+
+</ul>
+
+
+</div>
+
+
+        ) 
+
+
+    }
+}
